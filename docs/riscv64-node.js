ad9623bc4bd08c2cb840c468e3a1512d02a11850
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2023-03-16 17:38:38 +0900

    fix paths
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

--- docs/riscv64-node.js
+++ docs/riscv64-node.js
@@ -1,8 +1,8 @@
 importScripts("https://cdn.jsdelivr.net/npm/xterm-pty@0.9.4/workerTools.js");
 
 onmessage = (msg) => {
-  importScripts(location.origin + "/module.js");
-  importScripts(location.origin + "/containers/riscv64-node-container.js");
+  importScripts(location.origin + "/container2wasm-demo/module.js");
+  importScripts(location.origin + "/container2wasm-demo/containers/riscv64-node-container.js");
 
   emscriptenHack(new TtyClient(msg.data));
 };
